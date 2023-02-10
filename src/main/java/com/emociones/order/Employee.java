ee730529860d76c94fc59bd61fb0ef191feb5e9a
lgalindop <lgalindop@gmail.com>
2023-02-10 10:34:18 -0600

    Entity class Employee
    Signed-off-by: lgalindop <lgalindop@gmail.com>

--- src/main/java/com/emociones/order/Employee.java
+++ src/main/java/com/emociones/order/Employee.java
@@ -4,8 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
 
 @Data
 @Entity
